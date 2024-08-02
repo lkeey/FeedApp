ab9412a1e2440760b962992a2e235a6c69a1af3f
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2024-08-02 13:47:42 +0300

    add ability to work with network

---
+++ composeApp/src/commonMain/kotlin/features/feature_feed/CharacterCard.kt
@@ -0,0 +1,35 @@
+package features.feature_feed
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import features.feature_feed.models.Character
+
+@Composable
+fun CharacterCard(character: Character) {
+    Card {
+        Column {
+//            KamelImage(
+//                modifier = Modifier
+//                    .width(300.dp)
+//                    .height(200.dp),
+//                resource = asyncPainterResource(data = character.imageUrl),
+//                contentScale = ContentScale.Crop,
+//                contentDescription = "A picture of ${character.fullName}"
+//            )
+            Text(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                text = character.fullName,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/features/feature_feed/FeedScreen.kt
+++ composeApp/src/commonMain/kotlin/features/feature_feed/FeedScreen.kt
@@ -1,10 +1,48 @@
 package features.feature_feed
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import core.util.ScreenState
 
 @Composable
 fun FeedScreen(
-
+    viewModel: FeedViewModel = androidx.lifecycle.viewmodel.compose.viewModel { FeedViewModel() }
 ) {
-    FeedView()
+    val viewState by viewModel.uiState.collectAsState()
+
+    when (viewState.screenState) {
+        ScreenState.LOADING -> {
+            // handle loading animation
+        }
+
+        ScreenState.SUCCESS -> {
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize().padding(8.dp),
+                columns = GridCells.Adaptive(minSize = 128.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(viewState.characters) { character ->
+                    CharacterCard(character)
+                }
+            }
+        }
+
+        ScreenState.ERROR -> {
+            Text("Request failed")
+        }
+    }
+//    FeedView()
 }
+
+
